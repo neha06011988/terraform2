2f656293829c724c1f65e9b604640d49b628a5fd
neha06011988 <110978976+neha06011988@users.noreply.github.com>
2024-05-20 19:15:50 +0530

    3

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "base_label" {
 
 module "subnet_cidr" {
   source          = "hashicorp/subnets/cidr"
-  version         = "1.1.0"
+  version         = "1.0.0"  # Update to the latest version
   vpc_cidr_block  = var.vpc_cidr
   subnet_prefixes = ["24", "24"] # Two /24 subnets
 }
@@ -40,45 +40,4 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags                 = module.base_label.tags
-}
-
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = module.subnet_cidr.ipv4_subnet_cidrs[0]
-  availability_zone = data.aws_availability_zones.available.names[0] # Dynamic AZ
-  map_public_ip_on_launch = true # Enable public IP assignment
-  tags             = module.public_subnet.tags
-}
-
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = module.subnet_cidr.ipv4_subnet_cidrs[1]
-  availability_zone = data.aws_availability_zones.available.names[0] # Dynamic AZ
-  tags             = module.private_subnet.tags
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags   = module.base_label.tags
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = module.public_subnet.tags
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
 }
